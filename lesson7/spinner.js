b7fdc1e08c1def0c7722df14b59c8dcddd8c2846
OksanaKozhukh <oksana.kozhukh71090@gmail.com>
2020-02-19 23:34:27 +0200

    edit

--- lesson7/spinner.js
+++ lesson7/spinner.js
@@ -2,8 +2,20 @@ const arr = ['⇑','⇗', '⇒', '⇓', '⇙', '⇐', '⇖'];
 
 function spinner (ob) {
     console.clear();
-    ob.counter = (ob.counter + 1) % arr.length;
+    ob.counter = (ob.counter + 1);// % arr.length;
     console.log(arr[ob.counter]);
 }
 
-setInterval(spinner, 1000, {counter: 0});
+//setInterval(spinner, 1000, {counter: 0});
+
+
+function spinner2 (ob) {
+    console.clear();
+    console.log(`[${'@'.repeat(ob.counter)}${'-'.repeat(10 - ob.counter)}]`);
+    ob.counter = (ob.counter + 1) % 11;
+}
+
+setInterval(spinner2, 1000, {counter:0})
+
+`[${'@'.repeat(ob.counter)}${'-'.repeat(10-ob.counter)}]`
+
