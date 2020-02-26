b05ae406b878cdfdb565e85365149abb938b4db6
OksanaKozhukh <oksana.kozhukh71090@gmail.com>
2020-02-26 11:56:23 +0200

    edit 1

--- lesson11_HW/1/task1.js
+++ lesson11_HW/1/task1.js
@@ -1,6 +1,6 @@
 function reconstructTree (inorder, preorder) {
     let result = [];
-
+     
     function findRootElement (arr) {
         return arr[Math.floor(arr.length/2)];
     }
@@ -8,34 +8,31 @@ function reconstructTree (inorder, preorder) {
     function buildTree (arr) {
        let root = result[0] = findRootElement (arr);
        let left = result[1] = arr.slice(0, arr.indexOf(root));
-       let rigth = result[2] = arr.slice(arr.indexOf(root+1));
-       
+       let right = result[2] = arr.slice(arr.indexOf(root+1));
+       //if (left.length < 3) {left.push('')}
+       //if (right.length < 3) {right.push('')}
+                  
        return result;
     }
     
-    result = buildTree (inorder); 
+    result = buildTree (inorder); // [4, [1,2,3], [5,6, '']]
 
-    let result2 = [];
+    result = buildTree(inorder).slice(1).map(item => buildTree(item));
 
-    for (let i = 1; i < result.length; i++) {
-        result2.push(result[0]);
-        if (result[i].length > 1) {
-            result2.push(buildTree(result[i]));
-        }
-    }
+    // for (let i = 1; i < result.length; i++) {
+    //     result2.push(result[0]);
+    //     if (result[i].length > 1) {
+    //         result2.push(buildTree(result[i]));
+            
+    //     }
+    // }
 
     
     
-    return result2;
+    return result;
     
 };
 
 console.log(reconstructTree([1,2,3,4,5,6], [4,2,1,3,6,5]));
 
 [4,[2,[1,[],[]],[3,[],[]]],[6,[5,[],[]],[]]]
-
-// let left = result[1] = inorder.slice(0, inorder.indexOf(preorder[0]));
-// let right = result[2] = inorder.slice(inorder.indexOf(preorder[0]+1));
-// let firstRootElement = findRootElement (inorder);
-// let rootLeftElement = findRootElement (result[1]);
-// let rootRightElement = findRootElement (result[2]);
